18df0cd9a3bf61b9f3de8e863cb4b47ead45ff57
CofiKingston <74000580+CofiKingston@users.noreply.github.com>
2022-08-16 21:12:25 +0200

    .

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,6 @@ import React from "react";
 
 
 
-const Footer = () => <div style={{color:'AliceBlue', height:'50px', fontSize:'20px',display:'flex', justifyContent:'end', alignItems:'center'}}>Copyright &copy; Filip Kvesić</div>
+const Footer = () => <div style={{color:'AliceBlue', height:'20px', fontSize:'20px',display:'flex', justifyContent:'end', alignItems:'center'}}>Copyright &copy; {'Filip Kvesić --> Pinokio ne skidaj gačice'}</div>
 
 export default Footer;
